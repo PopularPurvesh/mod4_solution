4b1d8f1c47aab9584e37cb312794d2641b3abbd9
PopularPurvesh <79793276+PopularPurvesh@users.noreply.github.com>
2021-05-04 00:13:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeakernames[i]);
+    byeSpeaker(names[i]);
   } else {
     helloSpeaker(names[i]);
   }
